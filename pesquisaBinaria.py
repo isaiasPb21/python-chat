167ca4946384a5740dbe3862ac2b81e52d0edb63
isaiasPb21 <57261898+isaiasPb21@users.noreply.github.com>
2019-11-25 14:15:08 -0300

    Update and rename bb.py to pesquisaBinaria.py

--- pesquisaBinaria.py
+++ pesquisaBinaria.py
@@ -1,7 +1,9 @@
+# Busca binária
 
-# Busca binária 
+# Lista base
 lista_numerica = [0,1,2,3,4,5,6,7,8,9,10,11,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
+# Função que encontra, caso existir, o indice do objeto procurado
 def busca_binaria(lista_numerica, numero_a_ser_buscado):
     primeiro = 0
     ultimo = len(lista_numerica) -1
@@ -16,9 +18,8 @@ def busca_binaria(lista_numerica, numero_a_ser_buscado):
                 ultimo = meio - 1
             else :
                 primeiro = meio + 1
-    return "Shit"
-
+    return "Não encontrado"
             
 print(lista_numerica)
 entrada_numero_busca = int(input("Digite um número a ser procurado na lista acima :\n"))
-print('Index : ', busca_binaria(lista_numerica, entrada_numero_busca))
+print("Index : ", busca_binaria(lista_numerica, entrada_numero_busca))
