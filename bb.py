033d9de08ea10b686b100256a31a4e0c6e8f6f61
isaiasPb21 <57261898+isaiasPb21@users.noreply.github.com>
2019-11-25 14:13:11 -0300

    Add files via upload

---
+++ bb.py
@@ -0,0 +1,24 @@
+
+# Busca binária 
+lista_numerica = [0,1,2,3,4,5,6,7,8,9,10,11,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+
+def busca_binaria(lista_numerica, numero_a_ser_buscado):
+    primeiro = 0
+    ultimo = len(lista_numerica) -1
+
+    while (primeiro <= ultimo) :
+        meio = ((primeiro + ultimo) // 2)
+
+        if (numero_a_ser_buscado == lista_numerica[meio]) :
+            return lista_numerica.index(numero_a_ser_buscado) 
+        else :
+            if (numero_a_ser_buscado < lista_numerica[meio]) :
+                ultimo = meio - 1
+            else :
+                primeiro = meio + 1
+    return "Shit"
+
+            
+print(lista_numerica)
+entrada_numero_busca = int(input("Digite um número a ser procurado na lista acima :\n"))
+print('Index : ', busca_binaria(lista_numerica, entrada_numero_busca))
